b75429b960a043581744266f0f142215a17d991a
mmm <www.mirabbos22@gmail.com>
2023-10-14 14:56:00 +0500

    work is done

---
+++ java.js
@@ -0,0 +1,70 @@
+let question = + prompt ("how old are you ?")
+if (question <= 12) {
+    alert ("You are so young kuchi kuchi")
+}else if (question <= 18 ) {
+    alert ("you must to work !")
+}else if (question <= 30 ) {
+    alert ("you about to have a rest ")
+}else if (question <= 60 ) {
+    alert ("you are having a rest ")
+}else if (question <= 130) {
+    alert ("What a person are you ")
+}else if (question <= 1000) {
+    alert ("you are the terminator ")
+}else if (question <= 1000000) {
+    alert ("DONT LIE")
+}else {
+    alert ("this in not a number")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mmm
